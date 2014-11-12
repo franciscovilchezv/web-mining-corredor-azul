712cc39aa787b11ce76703b1e610766fc371e32c
wilchess26 <vilchez.francisco@pucp.pe>
2014-11-12 01:15:18 -0500

    Ultimas ediciones

--- Parsing/Stream/streamParse.py
+++ Parsing/Stream/streamParse.py
@@ -11,23 +11,16 @@ from nltk.corpus import stopwords
 
 DELIMITER = '|'
 
-
-#ARCHIVO_ENTRADA = "Corredor Azul/Modificados/Dias/1/1.txt"
 ARCHIVO_SALIDA_ORIGINAL = "Corredor Azul/Modificados/Dias/corredor_azul_stream.csv"
 ARCHIVO_SALIDA = 'Corredor Azul/Modificados/Dias/output.csv'
 
 ARCHIVOS_ENTRADA = [
-    "Corredor Azul/Modificados/Dias/1/1.txt",
-    "Corredor Azul/Modificados/Dias/2/2.txt",
-    "Corredor Azul/Modificados/Dias/3/3.txt",
-    "Corredor Azul/Modificados/Dias/4/4.txt",
+#    "Corredor Azul/Modificados/Dias/1/1.txt",
+#    "Corredor Azul/Modificados/Dias/2/2.txt",
+#    "Corredor Azul/Modificados/Dias/3/3.txt",
+#    "Corredor Azul/Modificados/Dias/4/4.txt",
     "Corredor Azul/Modificados/Dias/5/5.txt"
 ]
-
-#ARCHIVO_ENTRADA = "Dia1/corredor_azul_stream.txt"
-#ARCHIVO_SALIDA_ORIGINAL = "Dia1/corredor_azul_stream.csv"
-#ARCHIVO_SALIDA = 'Dia1/output.csv'
-
 
 class Tweet:
 
@@ -37,6 +30,7 @@ class Tweet:
     date        = None
     esRT        = None
     estado      = None
+    cant_terminos = None
 
 
     def __init__(self,post):
@@ -55,6 +49,7 @@ class Tweet:
             self.esRT = 0
 
         self.estado      = 0 # Sin calificar
+        self.cant_terminos = 0 # se asignara el valor correcto luego de la limpieza
 
     
     def to_CSV(self):
@@ -64,23 +59,31 @@ class Tweet:
         output += str(self.body_tweet) + DELIMITER
         output += str(self.date) + DELIMITER
         output += str(self.esRT) + DELIMITER
-        output += str(self.estado) + "\n"
+        output += str(self.estado) + DELIMITER
+        output += str(self.cant_terminos) + "\n"
 
         return output
     
 
-    def to_sequence(self):
+    def to_sequence(self,tipo):
 
-        sequence = [self.id_tweet, self.id_author, self.body_tweet, self.date, self.esRT, self.estado]
+        if(tipo == 1):
+            sequence = [self.id_tweet, self.id_author, self.body_tweet, self.date, self.esRT, self.estado, self.cant_terminos]
+        else:
+            sequence = [self.id_tweet, self.id_author, self.body_tweet, self.date, self.esRT, str(0)]
 
         return sequence
 
 
-def sequenciar_tweets(vector_tweets_limpio):
+def sequenciar_tweets(vector_tweets_limpio,tipo):
     dev = []
 
-    for item in vector_tweets_limpio:
-        dev.append(item.to_sequence())
+    if(tipo == 1):
+        for item in vector_tweets_limpio:
+            dev.append(item.to_sequence(1))
+    else:
+        for item in vector_tweets_limpio:
+            dev.append(item.to_sequence(2))
 
     return dev
 
@@ -142,14 +145,40 @@ def limpieza_de_datos(vector_tweets):
 
         item = Tweet(var_json)
 
-
         # Eliminar RT
         if (item.esRT):
             continue
 
+
+        # Valorizacion automatica por los emoticones en el body_tweet :) :D =) =( :(
+        happy_faces = [':)', ':D', '=)', '(:', 'D:', '(=', ';)']
+        sad_faces = ['=(',':(','):', ')=']
+
+        felicidad = False
+        tristeza = False
+
+        for face in happy_faces:
+            if (face in item.body_tweet):
+                felicidad = True
+                break
+
+        for face in sad_faces:
+            if (face in item.body_tweet):
+                tristeza = True
+                break
+
+        if(tristeza and felicidad):
+            item.estado = 0
+        elif(felicidad):
+            item.estado = 1
+        elif(tristeza):
+            item.estado = -1
+        else:
+            item.estado = 0
+
+
         # Limpieza de Usuarios (Se procederá a eliminar a los usuarios en los textos)
         for user in var_json["entities"]["user_mentions"]:
-
             usuario = "@" + user["screen_name"]           
             item.body_tweet = item.body_tweet.replace(usuario.encode('utf-8'),'')
 
@@ -159,9 +188,17 @@ def limpieza_de_datos(vector_tweets):
             item.body_tweet = item.body_tweet.replace(url["url"].encode('utf-8'),'')
 
 
-        # Limpieza de hashtags (Se reemplaza #CorredorAzul por servicio y se eliminan los restantes)
+        # Limpieza de hashtags (Se reemplaza #CorredorAzul por servicio y tambien 'corredor azul' y 'corredorazul' y se eliminan los restantes)
+        item.body_tweet = item.body_tweet.decode('unicode_escape').encode('ascii','ignore')
 
         item.body_tweet = item.body_tweet.replace("#CorredorAzul","servicio")
+
+        insensitive_string = re.compile(re.escape('corredor azul'), re.IGNORECASE)
+        item.body_tweet = insensitive_string.sub('servicio', item.body_tweet)
+
+        insensitive_string = re.compile(re.escape('corredorazul'), re.IGNORECASE)
+        item.body_tweet = insensitive_string.sub('servicio', item.body_tweet)
+
         for tag in var_json["entities"]["hashtags"]:
             hashtag = "#" + tag["text"].encode('utf-8')
             item.body_tweet = item.body_tweet.replace(hashtag,'')
@@ -178,6 +215,16 @@ def limpieza_de_datos(vector_tweets):
         item.body_tweet = limpiezaNLTK(item.body_tweet)
         if (item.body_tweet == ""):
             continue
+
+
+        # Limpieza de palabras con longitud menor a 3
+        for palabra in item.body_tweet.split():
+            if (len(palabra) < 3):
+                item.body_tweet = item.body_tweet.replace(str(palabra),'')
+
+
+        # Guardar la cantidad de terminos que quedaron
+        item.cant_terminos = len(item.body_tweet.split())
 
         
         # Loading
@@ -202,8 +249,6 @@ def main():
     writer_original = csv.writer(output_original, delimiter=DELIMITER)
 
 
-
-
     for in_file in ARCHIVOS_ENTRADA:
 
         print "Procesando archivo: " + in_file
@@ -219,19 +264,17 @@ def main():
 
             if ('{' in line):
 
-                print line
                 try:
                     var_json        = json.loads(line)
                     vector_tweets.append(var_json)
                 except:
-                    print "* Tweet Corrupto"
                     pass
 
         vector_tweets_limpio    = limpieza_de_datos(vector_tweets)
-        vector_tweets_sequence  = sequenciar_tweets(vector_tweets_limpio)
+        vector_tweets_sequence  = sequenciar_tweets(vector_tweets_limpio,1)
 
         writer.writerows(vector_tweets_sequence)
-        writer_original.writerows(sequenciar_tweets(limpieza_retweets(vector_tweets)))    
+        writer_original.writerows(sequenciar_tweets(limpieza_retweets(vector_tweets),2))    
 
 
 
