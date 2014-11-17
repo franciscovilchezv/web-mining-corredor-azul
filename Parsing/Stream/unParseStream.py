a74b3c9712b135e22c8fe0cdb95d91d4ef05fcab
wilchess26 <vilchez.francisco@pucp.pe>
2014-11-16 20:07:47 -0500

    Tag Mac

--- Parsing/Stream/unParseStream.py
+++ Parsing/Stream/unParseStream.py
@@ -27,31 +27,31 @@ class Tweet:
 		self.estado = post[6]
 		self.cant_terminos = post[7]
 	
-	def to_CSV_adj(self):
-        output = ""
-        output += str(self.id_tweet) + DELIMITER
-        output += str(self.id_author) + DELIMITER
-        output += str(self.tweet_original) + DELIMITER
-        output += str(self.body_tweet) + DELIMITER
-        output += str(self.date) + DELIMITER
-        output += str(self.esRT) + DELIMITER
-        output += str(self.estado) + DELIMITER
-        output += str(self.cant_terminos) + DELIMITER
-		output += str(self.cant_adj) + "\n"
+	#def to_CSV_adj(self):
+	#	output = ""
+    #    output += str(self.id_tweet) + DELIMITER
+    #    output += str(self.id_author) + DELIMITER
+    #    output += str(self.tweet_original) + DELIMITER
+    #    output += str(self.body_tweet) + DELIMITER
+    #    output += str(self.date) + DELIMITER
+    #    output += str(self.esRT) + DELIMITER
+    #    output += str(self.estado) + DELIMITER
+    #    output += str(self.cant_terminos) + DELIMITER
+	#	output += str(self.cant_adj) + "\n"
 
-        return output
+ 	#   return output
 	
 	def to_CSV(self):
 		output = []
-		output.append(str(self.id_tweet)
-		output.append(str(self.id_author)
-		output.append(tweet_original)
-		output.append(body_tweet)
-		output.append(date)
-		output.append(str(esRT))
-		output.append(str(estado))
-		output.append(str(cant_terminos))
-		output.append(str(cant_adj))
+		output.append(str(self.id_tweet))
+		output.append(str(self.id_author))
+		output.append(self.tweet_original)
+		output.append(self.body_tweet)
+		output.append(self.date)
+		output.append(str(self.esRT))
+		output.append(str(self.estado))
+		output.append(str(self.cant_terminos))
+		output.append(str(self.cant_adj))
 		return output
 
 def main():
@@ -70,19 +70,24 @@ def main():
 	
 	#archivo de salida
 	output = open("Corredor Azul/Modificados/Dias/output_tagged.csv", 'wb')
-    filewriter = csv.writer(output, delimiter=DELIMITER)
-	
+	filewriter = csv.writer(output, delimiter=DELIMITER)
+
 	#importando el tagger en español de Stanford NLP
 	from nltk.tag.stanford import POSTagger
-	st = POSTagger('/usr/share/stanford-postagger-full-2014-08-27/models/spanish-distsim.tagger','/usr/share/stanford-postagger-full-2014-08-27/stanford-postagger-3.4.1.jar') 
+	#st = POSTagger('/Applications/XAMPP/htdocs/Proyectos/Stanford/stanford-postagger-full-2014-08-27/models/spanish-distsim.tagger','/Applications/XAMPP/htdocs/Proyectos/Stanford/stanford-postagger-full-2014-08-27/stanford-postagger-3.4.1.jar',encoding='utf-8') 
+	st = POSTagger('/Applications/XAMPP/htdocs/Proyectos/Stanford/stanford-postagger-full-2014-08-27/models/spanish.tagger','/Applications/XAMPP/htdocs/Proyectos/Stanford/stanford-postagger-full-2014-08-27/stanford-postagger-3.4.1.jar',encoding='utf-8') 
 	
 	for tweet in tweets:		
+		#print tweet.body_tweet
 		#Ejemplo: st.tag('What is the airspeed of an unladen swallow ?'.split())
 		tweet_tagged = st.tag((tweet.body_tweet).split())
 		#Ejem_output: [('What', 'WP'), ('is', 'VBZ'), ('the', 'DT'), ('airspeed', 'NN'), ('of', 'IN'), ('an', 'DT'), ('unladen', 'JJ'), ('swallow', 'VB'), ('?', '.')]
+		#print tweet_tagged
 		n_adj = 0
 		for tag in tweet_tagged:
-			if tag[1] == 'JJ': n_adj+=1
+			if tag[1] == 'rg': 
+				#print tag[1]
+				n_adj+=1
 		tweet.cant_adj = n_adj
 		filewriter.writerow(tweet.to_CSV())
 		
