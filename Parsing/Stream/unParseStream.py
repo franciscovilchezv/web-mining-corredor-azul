0275deb7792551b6cfa9837d16b333a571eeadec
fenartcos <arturo.oncevay@gmail.com>
2014-11-18 20:55:38 -0500

    Tagging words
    Nuevo campo en el output con el tag completo

--- Parsing/Stream/unParseStream.py
+++ Parsing/Stream/unParseStream.py
@@ -7,28 +7,29 @@ DELIMITER = '|'
 
 class Tweet:
 
-	id_tweet    = None
-	id_author   = None
-	tweet_original = None
-	body_tweet  = None
-	date        = None
-	esRT        = None
-	estado      = None
-	cant_terminos = None
-	cant_adj = None
+    id_tweet    = None
+    id_author   = None
+    tweet_original = None
+    body_tweet  = None
+    date        = None
+    esRT        = None
+    estado      = None
+    cant_terminos = None
+    cant_adj = None
+    tweet_tagged = None
 
-	def __init__(self,post):
-		self.id_tweet    = post[0]
-		self.id_author   = post[1]
-		self.tweet_original = post[2]
-		self.body_tweet  = post[3]
-		self.date        = post[4]
-		self.esRT = post[5]
-		self.estado = post[6]
-		self.cant_terminos = post[7]
-	
-	#def to_CSV_adj(self):
-	#	output = ""
+    def __init__(self,post):
+        self.id_tweet    = post[0]
+        self.id_author   = post[1]
+        self.tweet_original = post[2]
+        self.body_tweet  = post[3]
+        self.date        = post[4]
+        self.esRT = post[5]
+        self.estado = post[6]
+        self.cant_terminos = post[7]
+    
+    #def to_CSV_adj(self):
+    #    output = ""
     #    output += str(self.id_tweet) + DELIMITER
     #    output += str(self.id_author) + DELIMITER
     #    output += str(self.tweet_original) + DELIMITER
@@ -37,60 +38,66 @@ class Tweet:
     #    output += str(self.esRT) + DELIMITER
     #    output += str(self.estado) + DELIMITER
     #    output += str(self.cant_terminos) + DELIMITER
-	#	output += str(self.cant_adj) + "\n"
+    #    output += str(self.cant_adj) + "\n"
 
- 	#   return output
-	
-	def to_CSV(self):
-		output = []
-		output.append(str(self.id_tweet))
-		output.append(str(self.id_author))
-		output.append(self.tweet_original)
-		output.append(self.body_tweet)
-		output.append(self.date)
-		output.append(str(self.esRT))
-		output.append(str(self.estado))
-		output.append(str(self.cant_terminos))
-		output.append(str(self.cant_adj))
-		return output
+    #   return output
+    
+    def to_CSV(self):
+        output = []
+        output.append(str(self.id_tweet))
+        output.append(str(self.id_author))
+        output.append(self.tweet_original)
+        output.append(self.body_tweet)
+        output.append(self.date)
+        output.append(str(self.esRT))
+        output.append(str(self.estado))
+        output.append(str(self.cant_terminos))
+        output.append(str(self.cant_adj))
+        output.append(self.tweet_tagged)
+        return output
 
 def main():
 
-	print "Inicio..."
-	with open("Corredor Azul/Modificados/Dias/output.csv",'rb') as csvfile:
-		lines = csvfile.readlines()
-	# En esta variable estan todos los tweets
-	tweets = []
-	for line in lines:
-		line = line.replace("\n","")
-		line = line.replace("\r","")
-		fields = line.split('|')	
-		tweet = Tweet(fields)
-		tweets.append(tweet)
-	
-	#archivo de salida
-	output = open("Corredor Azul/Modificados/Dias/output_tagged.csv", 'wb')
-	filewriter = csv.writer(output, delimiter=DELIMITER)
+    print "Inicio..."
+    with open("Corredor Azul/Modificados/Dias/output_final_AO.csv", 'rb') as csvfile:
+        lines = csvfile.readlines()
+    # En esta variable estan todos los tweets
+    tweets = []
+    for line in lines:
+        line = line.replace("\n","")
+        line = line.replace("\r","")
+        fields = line.split('|')    
+        tweet = Tweet(fields)
+        tweets.append(tweet)
+    
+    #archivo de salida
+    output = open("Corredor Azul/Modificados/Dias/output_tagged.csv", 'wb')
+    filewriter = csv.writer(output, delimiter=DELIMITER)
 
-	#importando el tagger en español de Stanford NLP
-	from nltk.tag.stanford import POSTagger
-	#st = POSTagger('/Applications/XAMPP/htdocs/Proyectos/Stanford/stanford-postagger-full-2014-08-27/models/spanish-distsim.tagger','/Applications/XAMPP/htdocs/Proyectos/Stanford/stanford-postagger-full-2014-08-27/stanford-postagger-3.4.1.jar',encoding='utf-8') 
-	st = POSTagger('/Applications/XAMPP/htdocs/Proyectos/Stanford/stanford-postagger-full-2014-08-27/models/spanish.tagger','/Applications/XAMPP/htdocs/Proyectos/Stanford/stanford-postagger-full-2014-08-27/stanford-postagger-3.4.1.jar',encoding='utf-8') 
-	
-	for tweet in tweets:		
-		#print tweet.body_tweet
-		#Ejemplo: st.tag('What is the airspeed of an unladen swallow ?'.split())
-		tweet_tagged = st.tag((tweet.body_tweet).split())
-		#Ejem_output: [('What', 'WP'), ('is', 'VBZ'), ('the', 'DT'), ('airspeed', 'NN'), ('of', 'IN'), ('an', 'DT'), ('unladen', 'JJ'), ('swallow', 'VB'), ('?', '.')]
-		#print tweet_tagged
-		n_adj = 0
-		for tag in tweet_tagged:
-			if tag[1] == 'rg': 
-				#print tag[1]
-				n_adj+=1
-		tweet.cant_adj = n_adj
-		filewriter.writerow(tweet.to_CSV())
-		
-	print "Done"
+    #importando el tagger en espaÃ±ol de Stanford NLP
+    from nltk.tag.stanford import POSTagger
+    st = POSTagger('/Applications/XAMPP/htdocs/Proyectos/Stanford/stanford-postagger-full-2014-08-27/models/spanish-distsim.tagger','/Applications/XAMPP/htdocs/Proyectos/Stanford/stanford-postagger-full-2014-08-27/stanford-postagger-3.4.1.jar',encoding='utf-8')
+    #st = POSTagger('/Applications/XAMPP/htdocs/Proyectos/Stanford/stanford-postagger-full-2014-08-27/models/spanish.tagger','/Applications/XAMPP/htdocs/Proyectos/Stanford/stanford-postagger-full-2014-08-27/stanford-postagger-3.4.1.jar',encoding='utf-8')
+    #st = POSTagger('C:\Data\stanford-postagger-full-2014-08-27\models\spanish.tagger', 'C:\Data\stanford-postagger-full-2014-08-27\stanford-postagger-3.4.1.jar', encoding='utf-8')
+
+    n=0
+    for tweet in tweets:
+        n+=1
+        print tweet.body_tweet
+        #Ejemplo: st.tag('What is the airspeed of an unladen swallow ?'.split())
+        tweet_tagged = st.tag((tweet.body_tweet).split())
+        #Ejem_output: [('What', 'WP'), ('is', 'VBZ'), ('the', 'DT'), ('airspeed', 'NN'), ('of', 'IN'), ('an', 'DT'), ('unladen', 'JJ'), ('swallow', 'VB'), ('?', '.')]
+        #print tweet_tagged
+        n_adj = 0
+        for tag in tweet_tagged:
+            if tag[1] == 'rg': 
+                #print tag[1]
+                n_adj+=1
+        tweet.cant_adj = n_adj
+        tweet.tweet_tagged = tweet_tagged
+        filewriter.writerow(tweet.to_CSV())
+        if n % 100 == 0: print n
+    print "Done"
+    close(output)
 
 main()
